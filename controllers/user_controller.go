f0230a4d87799cc80e0c536c7f9ee980f55d1fed
Atif <atif@monotote.com>
2023-06-22 19:23:38 +0500

    WIP

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -10,29 +10,6 @@ import (
 	"gorm.io/gorm"
 )
 
-type PaginationMeta struct {
-	CurrentPage int    `json:"current_page"`
-	From        int    `json:"from"`
-	LastPage    int    `json:"last_page"`
-	Path        string `json:"path"`
-	PerPage     int    `json:"per_page"`
-	To          int    `json:"to"`
-	Total       int    `json:"total"`
-}
-
-type PaginationLinks struct {
-	First string      `json:"first"`
-	Last  string      `json:"last"`
-	Prev  interface{} `json:"prev"`
-	Next  interface{} `json:"next"`
-}
-
-type UserListResponse struct {
-	Data  []models.User   `json:"data"`
-	Meta  PaginationMeta  `json:"meta"`
-	Links PaginationLinks `json:"links"`
-}
-
 func ListUsers(c *gin.Context) {
 	db := c.MustGet("db").(*gorm.DB)
 	page, _ := strconv.Atoi(c.DefaultQuery("page", "1"))
@@ -45,24 +22,10 @@ func ListUsers(c *gin.Context) {
 	db.Offset(offset).Limit(limit).Order("first_name").Find(&users)
 	db.Model(&models.User{}).Count(&total)
 
-	response := UserListResponse{
-		Data: users,
-		Meta: PaginationMeta{
-			CurrentPage: page,
-			From:        offset + 1,
-			LastPage:    int(total/int64(limit)) + 1,
-			Path:        c.Request.URL.Path,
-			PerPage:     limit,
-			To:          offset + len(users),
-			Total:       int(total),
-		},
-		Links: PaginationLinks{
-			First: helpers.GenerateURL(c.Request, 1),
-			Last:  helpers.GenerateURL(c.Request, int(total/int64(limit))+1),
-			Prev:  helpers.GeneratePrevURL(c.Request, page),
-			Next:  helpers.GenerateNextURL(c.Request, int(total/int64(limit))+1, page),
-		},
-	}
+	meta := helpers.GeneratePaginationMeta(page, limit, offset, int(total))
+	links := helpers.GeneratePaginationLinks(c.Request, meta.LastPage, page)
+
+	response := helpers.GenerateListResponse(users, meta, links)
 
 	c.JSON(http.StatusOK, response)
 
--- helpers/pagination.go
+++ helpers/pagination.go
@@ -15,14 +15,25 @@ type PaginationMeta struct {
 	Total       int    `json:"total"`
 }
 
+type PaginationLinks struct {
+	First string      `json:"first"`
+	Last  string      `json:"last"`
+	Prev  interface{} `json:"prev"`
+	Next  interface{} `json:"next"`
+}
+
+type ListResponse struct {
+	Data  interface{}     `json:"data"`
+	Meta  PaginationMeta  `json:"meta"`
+	Links PaginationLinks `json:"links"`
+}
+
 func GenerateURL(r *http.Request, page int) string {
 	scheme := "http"
 	if r.TLS != nil {
 		scheme = "https"
 	}
-
 	return scheme + "://" + r.Host + r.URL.Path + "?page=" + strconv.Itoa(page)
-
 }
 
 func GeneratePrevURL(r *http.Request, page int) interface{} {
@@ -37,4 +48,33 @@ func GenerateNextURL(r *http.Request, lastPage int, page int) interface{} {
 		return GenerateURL(r, page+1)
 	}
 	return nil
+}
+
+func GeneratePaginationMeta(page, limit, offset, total int) PaginationMeta {
+	return PaginationMeta{
+		CurrentPage: page,
+		From:        offset + 1,
+		LastPage:    int(total/limit) + 1,
+		Path:        "",
+		PerPage:     limit,
+		To:          offset + limit,
+		Total:       total,
+	}
+}
+
+func GenerateListResponse(data interface{}, meta PaginationMeta, links PaginationLinks) ListResponse {
+	return ListResponse{
+		Data:  data,
+		Meta:  meta,
+		Links: links,
+	}
+}
+
+func GeneratePaginationLinks(r *http.Request, lastPage, page int) PaginationLinks {
+	return PaginationLinks{
+		First: GenerateURL(r, 1),
+		Last:  GenerateURL(r, lastPage),
+		Prev:  GeneratePrevURL(r, page),
+		Next:  GenerateNextURL(r, lastPage, page),
+	}
 }
